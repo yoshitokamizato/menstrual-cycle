638222e420da0d5b7bb91800df3057d58c283011
T.Tsujii <ttakehisabt@gmail.com>
2019-10-09 13:03:32 +0900

    fixed login screen and add RSpec tests

---
+++ spec/system/sign_in_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe "Sessions", type: :system do
+
+  before do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  def get_url_from_email_text(mail)
+    body = mail.body.encoded
+    body[/http[^"]+/]
+  end
+
+  scenario "user successfully signs up" do
+    user = build(:user)
+    visit root_path
+    expect(page).to have_http_status '200'
+    expect(page).to have_selector '#nav-sign-in', text: 'ログイン'
+
+    find('#nav-sign-up', text: '新規登録').click
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: user.password
+    fill_in "user_password_confirmation", with: user.password
+    expect { click_button "新規登録" }.to change(User.all, :count).by(1)
+
+    expect(page).to have_content "新規登録が完了しました。"
+
+    click_link "ログアウト"
+    expect(page).to have_current_path root_path
+    expect(page).to have_content "ログアウトしました。"
+    expect(page).to have_no_selector '#nav-sign-out', text: 'ログアウト'
+    expect(page).to have_selector '#nav-sign-up', text: '新規登録'
+  end
+
+  scenario "user successfully signs in" do
+    user = create(:user)
+    visit root_path
+    expect(page).to have_http_status '200'
+    find('#nav-sign-in', text: 'ログイン').click
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: user.password
+    click_button "ログイン"
+    expect(page).to have_content "ログインしました。"
+  end
+
+  scenario "user successfully reissues password" do
+    user = create(:user)
+    visit root_path
+    expect(page).to have_http_status '200'
+    find('#nav-sign-in', text: 'ログイン').click
+    find('#devise-link-new-password', text: 'パスワードを忘れましたか?').click
+    fill_in "user_email", with: user.email
+    expect { click_button "パスワードの再設定方法を送信する" }.to change { ActionMailer::Base.deliveries.size }.by(1)
+    expect(page).to have_content "パスワードの再設定について数分以内にメールでご連絡いたします。"
+
+    mail = ActionMailer::Base.deliveries.last
+    link = get_url_from_email_text(mail)
+
+    visit link
+    fill_in "user_password", with: "123456"
+    fill_in "user_password_confirmation", with: "123456"
+    click_button "パスワードを変更する"
+    expect(page).to have_content "パスワードが正しく変更されました。"
+    expect(page).to have_selector '#nav-sign-out', text: 'ログアウト'
+  end
+end
