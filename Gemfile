e169b1ff79206cea76f5afe1c95bc2b8c3ac17f0
keisukeadachi <c3a578zx@gmail.com>
2019-10-12 23:17:23 +0900

    add_meal_CRUD

--- Gemfile
+++ Gemfile
@@ -21,6 +21,8 @@ gem 'devise-i18n-views'
 gem 'devise-bootstrap-views'
 gem 'jquery-rails'
 gem 'rails-i18n'
+gem 'carrierwave'
+gem 'rmagick'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_000815) do
+ActiveRecord::Schema.define(version: 2019_10_12_111643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meals", force: :cascade do |t|
+    t.string "menstrual_cycle"
+    t.string "image"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
