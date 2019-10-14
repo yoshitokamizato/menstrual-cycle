317f8d8fc8a85cd80bfdbfc2abebae0c40fbbc43
keisukeadachi <c3a578zx@gmail.com>
2019-10-14 16:37:10 +0900

    add redirect to login page

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,6 @@
 class ExercisesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @exercises = Exercise.all
   end
--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,4 +1,6 @@
 class MealsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @meals = Meal.all
   end
