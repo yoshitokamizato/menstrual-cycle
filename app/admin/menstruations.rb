8ef40221e292147ab6c06664ad25e59ed4d370c2
T.Tsujii <ttakehisabt@gmail.com>
2019-11-03 08:13:34 +0900

    add tables to ActiveAdmin

---
+++ app/admin/menstruations.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Menstruation do
+  permit_params :name
+end
--- app/models/menstruation.rb
+++ app/models/menstruation.rb
@@ -4,7 +4,8 @@ class Menstruation < ApplicationRecord
 
   def self.my_menstruation(date)
     menstrual_cycle = ((Date.today - date).to_i % 28) / 7
-    menstrual_cycle = 2 if menstrual_cycle == 3
+    # 生理周期を３つに分ける場合は，黄体期を１４日分とする
+    menstrual_cycle = 2 if Menstruation.count == 3 && menstrual_cycle == 3
     Menstruation.all[menstrual_cycle]
   end
 end
